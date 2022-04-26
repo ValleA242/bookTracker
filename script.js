0fcf19f865197ffb5f1b422b6a7fb76a214da4bc
Alicio Valle <71409214+ValleA242@users.noreply.github.com>
2022-04-25 22:46:20 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  console.log(choice)
+  const url = `https://openlibrary.org/isbn/${choice}.json`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        document.querySelector('h2').innerText = data.title
+        document.querySelector('img').innerHTML = data.covers[0]
+      })
+
+      
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
